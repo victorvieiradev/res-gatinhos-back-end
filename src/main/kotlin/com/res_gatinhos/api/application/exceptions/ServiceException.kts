84a475389a9f2489ae717b0a51790a36b3f7e37d
CLeitonEsilva <cleiton.ederson.silva@gmail.com>
2025-03-18 23:38:29 -0300

    fiz uma dto e alguns exception

--- src/main/kotlin/com/res_gatinhos/api/application/exceptions/ServiceException.kts
+++ src/main/kotlin/com/res_gatinhos/api/application/exceptions/ServiceException.kts
@@ -1,4 +1,3 @@
 package com.res_gatinhos.api.application.exceptions
 
 class ServiceException
-    (message: String = "Falha interna no serviço") : RuntimeException(message)
--- src/main/kotlin/com/res_gatinhos/api/application/exceptions/whatsAppCommunicationException.kts
+++ src/main/kotlin/com/res_gatinhos/api/application/exceptions/whatsAppCommunicationException.kts
@@ -1,6 +1,4 @@
 package com.res_gatinhos.api.application.exceptions
 
 
-class WhatsAppCommuncationException
-    (message: String = "Falha ao enviar mensagem WhatsApp") : RuntimeException(message)
 
